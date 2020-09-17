dee3e7913d51dee0644de237873d8df6c921795c
Michael Huber <michael.huber@hellocash.at>
2020-09-17 15:59:04 +0200

    lazy loading of tenant_id from jwt

--- src/Scopes/TenantScope.php
+++ src/Scopes/TenantScope.php
@@ -7,14 +7,21 @@ namespace HelloCash\HelloMicroservice\Scopes;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Scope;
 use Illuminate\Database\Eloquent\Builder;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class TenantScope implements Scope
 {
 
-    public static $tenantId;
+    /**
+     * @var int|null
+     */
+    public static ?int $tenantId;
 
     public function apply(Builder $builder, Model $model): void
     {
+        if (is_null(self::$tenantId) && !is_null(JWTAuth::getToken())) {
+            self::$tenantId = JWTAuth::parseToken()->getPayload()['tid'] ?? null;
+        }
         if (is_null(self::$tenantId)) {
             throw new \Exception('Tenant ID is missing!');
         }
